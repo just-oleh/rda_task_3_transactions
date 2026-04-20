c08820acb18370a97fd9909f27bea4ac23fbd795
Oleh Musurivskyi <musurivskiioleg@gmail.com>
2026-04-21 00:22:37 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,15 +1,21 @@
 USE ShopDB;
 
+START TRANSACTION;
+
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
-START TRANSACTION;
+SET @order_id = LAST_INSERT_ID();
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (1, 1, 1);
+VALUES (@order_id, 1, 1);
 
 UPDATE Products
-SET WarehouseAmount = WarehouseAmount - 1
+SET WarehouseAmount = WarehouseAmount - (
+    SELECT Count
+    FROM OrderItems
+    WHERE OrderID = @order_id AND ProductID = 1
+)
 WHERE ID = 1;
 
 COMMIT;
